d8b46d5a27c640b8111795160e566f99262fcd46
dymafr <erwan1309@hotmail.com>
2020-12-09 15:28:02 +0100

    Initial commit

---
+++ src/app/fruit/fruit.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+
+@Component({
+  selector: "app-fruit",
+  templateUrl: "./fruit.component.html",
+  styleUrls: ["./fruit.component.scss"]
+})
+export class FruitComponent implements OnInit {
+  @Input() public fruit: string;
+  @Output() private deleteF: EventEmitter<string> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  deleteFruit() {
+    this.deleteF.emit(this.fruit);
+  }
+}
