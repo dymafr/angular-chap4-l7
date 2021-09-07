7ccb68a4c8dda5e81510cb33b7ea7faaaea4822d
dymafr <erwan1309@hotmail.com>
2021-09-07 12:21:50 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,23 @@ import {
   QueryList,
   ViewChild,
   ViewChildren
-} from "@angular/core";
-import { FruitComponent } from "./fruit/fruit.component";
+} from '@angular/core';
+import { FruitComponent } from './fruit/fruit.component';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public fruits: string[] = ["fraise", "kiwi"];
-  @ViewChild("myinput") public el: ElementRef<HTMLInputElement>;
-  @ViewChildren(FruitComponent) public list: QueryList<FruitComponent>;
+  public fruits: string[] = ['fraise', 'kiwi'];
+  @ViewChild('myinput') public el!: ElementRef<HTMLInputElement>;
+  @ViewChildren(FruitComponent) public list!: QueryList<FruitComponent>;
 
   public addFruit() {
     console.info(this.list);
     this.fruits.push(this.el.nativeElement.value);
-    this.el.nativeElement.value = "";
+    this.el.nativeElement.value = '';
   }
 
   public deleteFruit(fruit: string) {
--- src/app/fruit/fruit.component.ts
+++ src/app/fruit/fruit.component.ts
@@ -1,12 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: "app-fruit",
-  templateUrl: "./fruit.component.html",
-  styleUrls: ["./fruit.component.scss"]
+  selector: 'app-fruit',
+  templateUrl: './fruit.component.html',
+  styleUrls: ['./fruit.component.scss']
 })
 export class FruitComponent implements OnInit {
-  @Input() public fruit: string;
+  @Input() public fruit?: string;
   @Output() private deleteF: EventEmitter<string> = new EventEmitter();
 
   constructor() {}
